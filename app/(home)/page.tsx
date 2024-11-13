1f4e26057a0f72781ef9f1d69a8ebfcd2f2bbc01
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-13 10:51:34 +0900

    client side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,23 @@
-export const metadata = {
-    title: 'Home',
-}
+"use client"
+
+import { useEffect, useState } from "react"
 
 export default function Page() {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const getMovies = async () => {
+        const res = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+        const json = await res.json();
+
+        setMovies(json);
+        setIsLoading(false);
+    }
+    useEffect(() => {
+        getMovies();
+    }, []);
     return (
         <div>
-            <h1>Home</h1>
+            {isLoading ? "Loading..." : JSON.stringify(movies)}
         </div>
     )
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,3 +2,11 @@ export default async function MovieDetail({ params }) {
     const { id } = await params;
     return <h1>Movie {id}</h1>
 }
+
+function MovieDetail1({
+    props: { id }
+}: {
+    props: { id: string }
+}) {
+    return <h1>Movie {id}</h1>
+}
